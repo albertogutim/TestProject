4e5a90ca690a24dd51daa461de645da7064d9dbf
Alberto Gutiérrez Martínez <albertogutim@MacBook-Air-de-Alberto.local>
2012-08-02 21:26:29 +0200

    Initial Commit

---
+++ TestProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by Alberto Gutiérrez on 02/08/12.
+//  Copyright (c) 2012 FLAG Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
